5aa5b07ee32fada573841f47d092b723727c83ab
elnacabot <ls30917@salleurl.edu>
2018-04-19 20:38:37 +0200

    Escenari casi preparat

---
+++ Betrayal_Vr/Assets/Scripts/cajon_script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cajon_script : MonoBehaviour {
+    public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.Play("abrir_cajon");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.Play("cerrar_cajon");
+        }
+    }
+}
